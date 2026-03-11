cee134275f241ab965aa1bb82bcd1baff9760456
adityapund <pundaditya62@gmail.com>
2026-03-12 01:26:56 +0530

    v1

--- DVC/mycode.py
+++ DVC/mycode.py
@@ -11,10 +11,6 @@ data = {"Name":["Aditya", "Kashish", "Didii"],
 
 df = pd.DataFrame(data)
 
-
-new_row = {"Name":"ABC", "Age":"20", "Mobile":"HTC"}
-df.loc[len(df.index)]=new_row
-
 data_dir = "data"
 os.makedirs(data_dir, exist_ok=True)
 
